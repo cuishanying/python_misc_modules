8cb4765be0b366f88d09ba14a00b59923eb7aff0
Shanying Cui <scui@seas.harvard.edu>
2013-04-16 10:47:37 -0400

    Refactored and first test code
    two classes: Spectrum and Map.

--- NVanalysis.py
+++ NVanalysis.py
@@ -4,197 +4,311 @@
 
 import pylab as py
 import winspec
+from numpy import ndarray
 
-def load_520exc(files,NV0fitrange=[573,586], NVmfitrange=[636,648]):
+class Spectrum():
     """
-    files: list
+    This class loads a .SPE file or .txt file
+    for .txt file (saved from Clarke's Raman), 
+    first column is wavelength and 2nd column is luminescence
     """
-    data=[winspec.Spectrum(f) for f in files]
     
-    NV=py.zeros((py.size(data),3))
-    NV[:,0]=[int_peak(d.wavelen, d.lum, fitrange=NV0fitrange) for d in data]
-    NV[:,1]=[int_peak(d.wavelen, d.lum,fitrange=NVmfitrange) for d in data]
-    NV[:,2]=NV[:,1]/(NV[:,0]+NV[:,1])
+    """ String for .txt filename """
+    _fname = ''
+    
+    """ List of wavelengths in this spectrum"""
+    _wavelen = []
+    
+    """ List of luminescenses associated with the wavelength at each index""" 
+    _lum = []
+    
+    """ Boolean to determine if file has been saved from low-to-high wavelengths"""
+    _reverse = False
+    
+    def __init__(self,*args):
+        if len(args) == 1:
+            assert type(args[0]) is str, "input must be either one str, or 2 lists"
             
-    return NV
-    
-def load_highgrating(files,NV0fitrange=[568,580],NV0intrange=[574,580],NVmfitrange=None):
-    """
-    files: list
-    format: str
-    """
-    dataNV0=[]
-    dataNVm=[]
-    for f in files:
-        if f.find('NV0') is not -1:
-            dataNV0.append(winspec.Spectrum(f))
-        elif f.find('NVm') is not -1:
-            dataNVm.append(winspec.Spectrum(f))
-        else:
-            raise ValueError(f,'filename must contain either "NV0" or "NVm"')
-    
-    data=py.vstack([dataNV0,dataNVm])
-    NV=py.zeros((py.size(dataNV0),3))
-
-    NV[:,0]=[int_peak(d.wavelen, d.lum, fitrange=NV0fitrange,intrange=NV0intrange) for d in dataNV0]
-    NV[:,1]=[int_peak(d.wavelen, d.lum,fitrange=NVmfitrange) for d in dataNVm]
-    NV[:,2]=NV[:,1]/(NV[:,0]+NV[:,1])
+            self._fname = args[0]
+            self._wavelen = py.loadtxt(self._fname)[:,0]    
+            if self._wavelen[0] > self._wavelen[-1]:
+                self._wavelen = self._wavelen[::-1]
+                self._reverse=True
+                
+            self._lum = py.loadtxt(self._fname)[:,1]
+            if self._reverse is True:
+                self._lum = self._lum[::-1]
+                
+            assert len(self._wavelen) == len(self._lum), "Wavelength and luminescence length should match"
+        elif len(args) == 2:
+            assert type(args[0]) is ndarray or type(args[0]) is list, "first input must be list or ndarray"
+            assert type(args[1]) is ndarray or type(args[0]) is list, "second input must be list or ndarray"
             
-    return NV
-
-def find_focus (filename, maxwavelen=638):
-    wavelen=py.loadtxt(filename)[0]
-    data=py.loadtxt(filename,skiprows=1)
-    
-    if wavelen[0] > wavelen[-1]:
-        reverse=True
-        NVmindex=py.where(wavelen>maxwavelen)[0][-1]+1
-    else:
-        reverse=False
-        NVmindex=py.where(wavelen>maxwavelen)[0][0]+1
-    
-    NVint=data[0][NVmindex]
-    bestfocus=0       
-    for focus in py.arange(py.shape(data)[0]):
-        if data[focus][NVmindex] > NVint:
-            NVint=data[focus][NVmindex]
-            bestfocus=data[focus][0]
-    print 'best focus is ', bestfocus        
-    return wavelen, data[bestfocus][1:]
-
-def find_focus_map (filename, npoints, maxwavelen=638) :
-    """
-    returns wavelength, list of lum where each row is a different point 
-    
-    filename: str
-        txt filename
-        first row: wavelen
-        2nd to end row: luminescence data, including scan properties (len = len(wavelen)+3)
-        col0: z, col1 and 2: x and y, col3 to end: luminescence
-    npoints: total number of points (x and y)
-    
-    returns: wavelen (low to high), array of luminescence at best focus
-    """
-    wavelen=py.loadtxt(filename)[0]
-    data=py.loadtxt(filename,skiprows=1)
-    
-    # sort
-    data=list(data)
-    data.sort( key=lambda x: x[1])
-    data.sort( key=lambda x: x[2])
-    nfocus=len(data)/npoints
-    data=py.asarray(data)
-    
-    # find index, depending on if saved data goes from low to high or high to low in wavelength
-    if wavelen[0] > wavelen[-1]:
-        reverse=True
-        NVmindex=py.where(wavelen>maxwavelen)[0][-1]+3
-    else:
-        reverse=False
-        NVmindex=py.where(wavelen>maxwavelen)[0][0]+3
-    
-    # find index of maximum NV intensity per xy-point
-    NVmintensity=py.zeros(npoints)
-    maxindex=py.zeros(npoints)
-    
-    row=0
-    while row < data.shape[0]:
-        for point in py.arange(npoints):
-            for focus in py.arange(nfocus):
-                readNVm=data[row,NVmindex]
-                if readNVm>NVmintensity[point]:
-                    NVmintensity[point]=readNVm
-                    maxindex[point]=row
-                row+=1
-
-    # save and return wavelen (from low to high), luminescence
-    if reverse is True:
-        wavelen=wavelen[::-1]
-
-    maxdata=[]
-    maxfocus=[]
-    for point in maxindex:
-        maxfocus.append(data[point][0])
-        if reverse is True:
-            maxdata.append(data[point][3:][::-1])
-        else:
-            maxdata.append(data[point][3:])
-    print 'focus is ', maxfocus
-    return wavelen, maxdata
-
-def plot(wavelen, maxdata, *args, **kwargs):
-    for i in range(len(maxdata)):
-        if i > 6:
-            fmt = '--'
-        else:
-            fmt = '-'
-        py.plot(wavelen, maxdata[i],label=str(i),linestyle=fmt)
-    py.legend()
-            
-def int_peak (wavelen, lum, fitrange=None, intrange=None, normalize=False, plot=False, npoints=10):
-    """
-    Fits a linear background, subtracts the background, and integrates. Intended to be used for integrating peaks.
-    
-    wavelen : list
-        list of wavelengths in nm. Can be sorted from low to high or high to low
-    lum : list
-        list of luminescence
-    fitrange : 2-element list, optional
-        Defaults to the span of the data. Input: [low nm, high nm]
-    intrange : 2-element list, optional
-        Defaults to the span of the data or fitrange (if given). Input: [low nm, high nm]
-    normalize : boolean, optional
-        Default is False
-    plot : boolean, optional
-        Default is False. Plots the original data, the linear background, and the data with the background subtracted
-    npoints : int
-        Default is 10. Number of points above and below the given fitrange point to average over.
-    """
-    if wavelen[0]>wavelen[-1]:
-        wavelen=wavelen[::-1]
-        lum=lum[::-1]
+            self._wavelen = args[0]
+            self._lum = args[1] 
         
-    if normalize is True:
-        lum/=lum.max()
+    def get_wavelen(self):
+        return self._wavelen
     
-    if fitrange is None:
-        fitindex=[0+npoints/2, len(wavelen)-1-npoints/2]
-    else:
-        fitindex=[0, 0]
-        fitindex[0]=py.where(wavelen>fitrange[0])[0][0]
-        fitindex[1]=py.where(wavelen>fitrange[1])[0][0]
+    def get_lum(self):
+        return self._lum
     
-    wavelenfit=py.concatenate((wavelen[fitindex[0]-npoints/2:fitindex[0]+npoints/2], 
-                           wavelen[fitindex[1]-npoints/2:fitindex[1]+npoints/2]))
-    lumfit=py.concatenate((lum[fitindex[0]-npoints/2:fitindex[0]+npoints/2], 
-                        lum[fitindex[1]-npoints/2:fitindex[1]+npoints/2]))
-    linearfit = py.polyfit(wavelenfit, lumfit, 1)
-    linear_bg = py.polyval( linearfit, wavelen[fitindex[0]:fitindex[1]+1] )
-    wavelen_bg = wavelen[fitindex[0]:fitindex[1]+1].copy()
-    lum_bg = lum[fitindex[0]:fitindex[1]+1].copy()
-    lum_bg -= linear_bg
+    def get_lumAtWavelen(self, wavelen):
+        lum = None
+        index = py.where(self._wavelen > wavelen)[0][0]
+        lum = self._lum[index]
+            
+        assert lum != None, "wavelength out of bound"
+        return lum
     
-    if plot is True:
-        py.plot(wavelen,lum,'k')
-        py.plot(wavelen_bg,linear_bg,'k:')
-        py.plot(wavelen_bg,lum_bg,'r')
-        py.show()
+    def int_peak(self,fitrange=None, intrange=None, normalize=False, plot=False, npoints=10):
+        """
+        Fits a linear background, subtracts the background, and integrates. Intended to be used for integrating peaks.
+        
+        wavelen : list
+            list of wavelengths in nm. Can be sorted from low to high or high to low
+        lum : list
+            list of luminescence
+        fitrange : 2-element list, optional
+            Defaults to the span of the data. Input: [low nm, high nm]
+        intrange : 2-element list, optional
+            Defaults to the span of the data or fitrange (if given). Input: [low nm, high nm]
+        normalize : boolean, optional
+            Default is False
+        plot : boolean, optional
+            Default is False. Plots the original data, the linear background, and the data with the background subtracted
+        npoints : int
+            Default is 10. Number of points above and below the given fitrange point to average over.
+        """
+        if fitrange is None:
+            fitindex=[0+npoints/2, len(self._wavelen)-1-npoints/2]
+        else:
+            fitindex=[0, 0]
+            fitindex[0]=py.where(self._wavelen>fitrange[0])[0][0]
+            fitindex[1]=py.where(self._wavelen>fitrange[1])[0][0]
+        
+        wavelenfit=py.concatenate((self._wavelen[fitindex[0]-npoints/2:fitindex[0]+npoints/2], 
+                               self._wavelen[fitindex[1]-npoints/2:fitindex[1]+npoints/2]))
+        lumfit=py.concatenate((self._lum[fitindex[0]-npoints/2:fitindex[0]+npoints/2], 
+                            self._lum[fitindex[1]-npoints/2:fitindex[1]+npoints/2]))
+        linearfit = py.polyfit(wavelenfit, lumfit, 1)
+        linear_bg = py.polyval( linearfit, self._wavelen[fitindex[0]:fitindex[1]+1] )
+        wavelen_bg = self._wavelen[fitindex[0]:fitindex[1]+1].copy()
+        lum_bg = self._lum[fitindex[0]:fitindex[1]+1].copy()
+        lum_bg -= linear_bg
+        
+        if plot is True:
+            py.plot(self._wavelen,self._lum,'k')
+            py.plot(wavelen_bg,linear_bg,'k:')
+            py.plot(wavelen_bg,lum_bg,'r')
+            py.show()
+        
+        intindex=[0,0]
+        if intrange is None:
+            wavelen_int = wavelen_bg
+            lum_int = lum_bg  
+        else:
+            intindex[0]=py.where(wavelen_bg>intrange[0])[0][0]
+            intindex[1]=py.where(wavelen_bg>intrange[1])[0][0]    
+            wavelen_int = wavelen_bg[intindex[0]:intindex[1]+1]
+            lum_int = lum_bg[intindex[0]:intindex[1]+1]
+        
+        peak_area = py.trapz(lum_int, x=wavelen_int)
+        return peak_area
+        
+    def get_NVratio(self, NV0fitrange = [567, 581], NV0intrange = [573.5, 581], NVmfitrange = [633, 644],plot=False):
+        """
+        Returns a list of NV0 ZPL area, NVm ZPl area, and NVm/(NV0+NVm) ratio.
+        
+        NV0fitrange: list, optional
+            Default [567, 581] (nm)
+        NV0intrange: list, optional
+            Default [573.5, 581] (nm)
+        NVmfitrange: list, optional
+            Default [633, 644] (nm)
+        plot: boolean, optional
+            Default False
+        """
+        NV0=self.int_peak(fitrange=NV0fitrange, intrange=NV0intrange, plot=plot)
+        NVm=self.int_peak(fitrange=NVmfitrange,plot=plot)
+        
+        return [NV0,NVm,NVm/(NV0+NVm)]
     
-    intindex=[0,0]
-    if intrange is None:
-        wavelen_int = wavelen_bg
-        lum_int = lum_bg  
-    else:
-        intindex[0]=py.where(wavelen_bg>intrange[0])[0][0]
-        intindex[1]=py.where(wavelen_bg>intrange[1])[0][0]    
-        wavelen_int = wavelen_bg[intindex[0]:intindex[1]+1]
-        lum_int = lum_bg[intindex[0]:intindex[1]+1]
+    def plot(self,*args,**kwargs):
+        py.plot(self._wavelen, self._lum,*args,**kwargs)
+        
+#    def load_520exc(files,NV0fitrange=[573,586], NVmfitrange=[636,648]):
+#        """
+#        files: list
+#        """
+#        data=[winspec.Spectrum(f) for f in files]
+#        
+#        NV=py.zeros((py.size(data),3))
+#        NV[:,0]=[int_peak(d.wavelen, d.lum, fitrange=NV0fitrange) for d in data]
+#        NV[:,1]=[int_peak(d.wavelen, d.lum,fitrange=NVmfitrange) for d in data]
+#        NV[:,2]=NV[:,1]/(NV[:,0]+NV[:,1])
+#                
+#        return NV
+#
+#    def load_highgrating(files,NV0fitrange=[568,580],NV0intrange=[574,580],NVmfitrange=None):
+#        """
+#        files: list
+#        format: str
+#        """
+#        dataNV0=[]
+#        dataNVm=[]
+#        for f in files:
+#            if f.find('NV0') is not -1:
+#                dataNV0.append(winspec.Spectrum(f))
+#            elif f.find('NVm') is not -1:
+#                dataNVm.append(winspec.Spectrum(f))
+#            else:
+#                raise ValueError(f,'filename must contain either "NV0" or "NVm"')
+#        
+#        data=py.vstack([dataNV0,dataNVm])
+#        NV=py.zeros((py.size(dataNV0),3))
+#    
+#        NV[:,0]=[int_peak(d.wavelen, d.lum, fitrange=NV0fitrange,intrange=NV0intrange) for d in dataNV0]
+#        NV[:,1]=[int_peak(d.wavelen, d.lum,fitrange=NVmfitrange) for d in dataNVm]
+#        NV[:,2]=NV[:,1]/(NV[:,0]+NV[:,1])
+#                
+#        return NV
     
-    peak_area = py.trapz(lum_int, x=wavelen_int)
-    return peak_area
 
-def int_NVpeaks(wavelen, lum,NV0fitrange = [567, 581],NV0intrange = [573.5, 581],NVmfitrange = [633, 644],plot=False):
-    NV0=int_peak(wavelen, lum, fitrange=NV0fitrange, intrange=NV0intrange, plot=plot)
-    NVm=int_peak(wavelen, lum, fitrange=NVmfitrange,plot=plot)
+class Map():
     
-    return [NV0,NVm,NVm/(NV0+NVm)]
+    """ filename """
+    _fname = ''
+    _wavelen=[]
+    _specList=[]
+    _reverse=False
+    _z=[]
+    _nfocus=0
+    
+    def __init__(self,fname):
+        """
+        initializes Map object, and loads a z scan or x, y, z map. 
+        """
+        
+        self._fname = fname
+        self._wavelen = py.loadtxt(self._fname)[0]
+        if self._wavelen[0] > self._wavelen[-1]:
+            self._wavelen = self._wavelen[::-1]
+            self._reverse=True
+            
+        data = py.loadtxt(self._fname,skiprows=1)
+        
+        # check what kind of a file it is
+        lendiff = py.shape(data)[1] - len(self._wavelen)
+        if lendiff == 1:
+            self.load_focus(data)
+        if lendiff == 3:
+            self.load_map(data)
+    
+    def get_wavelen(self):
+        return self._wavelen
+    
+    def get_specList(self):
+        return self._specList
+    
+    def get_zvals(self):
+        return self._z
+    
+    def load_focus(self,data):
+        """
+        return list of Spectrum 
+        """
+        self._z=data[:,0]
+        self._nfocus=len(data[:,0])
+        for focus in data:
+            if self._reverse == True:
+                lum = focus[1:][::-1]
+            else:
+                lum = focus[1:]
+            self._specList.append(Spectrum(self._wavelen,lum))
+        
+        self._specList=[self._specList] # only one point
+        
+        assert len(self._specList[0]) == self._nfocus, "Num focus points must equal num of luminescence"
+        
+    def load_map(self,data):
+        
+        # sort
+        data=list(data)
+        data.sort( key=lambda x: x[1])
+        data.sort( key=lambda x: x[2])
+        data=py.asarray(data)
+        
+        # find nfocus
+        firstfocus=data[0][0]
+        for row in data[1:]:
+            self._nfocus+=1
+            if row[0] == firstfocus:
+                break
+        
+        # extract lum data    
+        for row in data:
+            if self._reverse == True:
+                lum = row[3:][::-1]
+            else:
+                lum = row[3:]
+            self._specList.append(Spectrum(self._wavelen,lum))
+
+        # split specList into points, ie. [[z1, z1, z3],[z1,z2,z3]]
+        self._specList=[self._specList[i:i+self._nfocus] for i in range(0,len(self._specList),self._nfocus)]
+        self._z = (data[:,0][0:self._nfocus])
+        
+        assert len(self._z) == len(self._specList[0]), "len of focuses must match specList sublist length"        
+
+    def find_focus(self, wavelen,plot=False):
+        """
+        within a list of spectrum objects of the same point and a wavelength, find the focus of highest intensity
+        Returns list index values where the lum is maximized for each point in map
+        
+        wavelen: int or float
+            Finds focus with max luminescence at this wavelength (nm) 
+        """
+        lum = []
+        maxlum_index = []
+        for point in self._specList:
+            if plot == True:
+                py.figure()
+            for spec in point:
+                lum.append(spec.get_lumAtWavelen(wavelen))
+                if plot == True:
+                    spec.plot()
+            maxlum_index.append(lum.index(max(lum)))
+            lum = []
+             
+        return  maxlum_index
+    
+    def get_NVratio(self,maxwavelen=638,*args,**kwargs):
+        """
+        Finds best focus at given wavelength for each point, 
+        
+        maxwavelen: int or float, optional
+            Default is 638nm, maximizing to NVm ZPL
+        """
+        NVvalues=[]
+        indexList = self.find_focus(maxwavelen)
+            
+        for i in py.arange(len(self._specList)):
+            focusedPoint = self._specList[i][indexList[i]]
+            NVvalues.append(focusedPoint.get_NVratio(*args,**kwargs))
+        
+        return NVvalues
+
+    def plot(self,maxdata, *args, **kwargs):
+        """ copied directly from previous version, not complete"""
+        for i in range(len(maxdata)):
+            if i > 6:
+                fmt = '--'
+            else:
+                fmt = '-'
+            py.plot(self._wavelen, maxdata[i],label=str(i),linestyle=fmt)
+        py.legend()
+        
+    def removeData(self):
+        """
+        Removes crappy spectra as necessary
+        """
---
+++ testcode/NVanalysis_test.py
@@ -0,0 +1,53 @@
+'''
+Created on Apr 15, 2013
+
+@author: Shanying
+'''
+
+import NVanalysis as nv
+from pylab import show, shape, mean, std, asarray
+
+def testSpectrum():
+    specFile = 'C:/Users/Shanying/Documents/Hu lab/Data/PL/130305-Igor-CF4_D0p6-200u-600g-50x-3s10x/2p5minO2_fullspectrum_D0p6-200u-100x-1s10x.txt'
+    spec=nv.Spectrum(specFile)
+    
+    #spec.plot()
+    #NV0area = spec.int_peak(fitrange = [567, 581], intrange = [573.5, 581],plot=True)
+    #print NV0area
+    NVratios = spec.get_NVratio(plot=True)
+    print NVratios
+    #show()
+
+class testFocusMap():
+
+    def testLoad(self):
+        mapFile = 'C:/Users/Shanying/Documents/Hu lab/Data/PL/130305-Igor-CF4_D0p6-200u-600g-50x-3s10x/2p5minO2_spot1_zscan.txt'
+        map=nv.Map(mapFile)      
+        print map.get_wavelen()
+        print map.get_specList()
+        print map.get_zvals()
+
+    def testNV(self):
+        mapFile = 'C:/Users/Shanying/Documents/Hu lab/Data/PL/130305-Igor-CF4_D0p6-200u-600g-50x-3s10x/2p5minO2_spot1_zscan.txt'
+        map=nv.Map(mapFile)
+        print map.get_NVratio()        
+        
+class testMap():
+    
+    def testLoad(self):
+        mapFile = 'C:/Users/Shanying/Documents/Hu lab/Data/PL/130328-Kanye-acid_D1-200um-600nm-600g-50xobj-1s10x/3x3map_z-8to8um.txt'
+        map=nv.Map(mapFile)
+        print map.get_wavelen()
+        print map.get_specList()
+        print map.get_zvals()
+    
+    def testNV(self):
+        mapFile = 'C:/Users/Shanying/Documents/Hu lab/Data/PL/130328-Kanye-acid_D1-200um-600nm-600g-50xobj-1s10x/3x3map_z-8to8um.txt'
+        map=nv.Map(mapFile)
+        NV= asarray(map.get_NVratio())
+        print mean(NV[:,2]), std(NV[:,2])
+        
+        # should be 0.586 +/- 0.013
+        
+test=testMap()
+test.testNV()
